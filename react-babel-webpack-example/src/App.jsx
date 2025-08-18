0fede0cb86e0450096990de2a4c544e54ec5e3c5
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-18 15:41:19 -0300

    feat: configurar estrutura inicial do React com App.jsx e renderização no index.js

---
+++ react-babel-webpack-example/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Projeto React com webpack e babel</h1>
+      <p>Este é um exemplo básico de aplicação React.</p>
+    </div>
+  );
+}
+
+export default App
