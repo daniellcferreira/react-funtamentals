e9b7e16351f0cd9effecf57b5641729a01eaef02
Daniel Ferreira <dcfer2017@gmail.com>
2025-08-21 11:05:49 -0300

    feat: criar componente Profile e atualizar App.tsx e App.css

---
+++ first-component/src/components/Profile.tsx
@@ -0,0 +1,14 @@
+export function Profile() {
+  return (
+    <div>
+      <img
+        src="https://avatars.githubusercontent.com/u/146852579?v=4"
+        alt="Daniel Ferreira"
+        width="300px"
+      />
+      <h3>Daniel Ferreira</h3>
+      <p>Full Stack Developer</p>
+      <p>Location: Brazil</p>
+    </div>
+  );
+}
